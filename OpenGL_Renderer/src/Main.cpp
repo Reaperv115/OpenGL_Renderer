9d11cbc1a6459c3a91652decda613b65c4b5f1ca
slaygony <rjs5785@hotmail.com>
2025-03-21 10:36:07 -0700

    triangle is drawing again and im displaying the versions of lgew and glfw being used

--- OpenGL_Renderer/src/Main.cpp
+++ OpenGL_Renderer/src/Main.cpp
@@ -10,9 +10,10 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+	else
+		std::cout << "GLFW Version: " << glfwGetVersionString() << std::endl;
 
-	if (!glewInit())
-		return -1;
+	
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -25,7 +26,27 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+		return -1;
+	else
+		std::cout << "GLEW Version: " <<  glewGetString(GLEW_VERSION) << std::endl;
+
+	float triangle[6] =
+	{
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangle, GL_STATIC_DRAW);
 	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -33,11 +54,9 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
