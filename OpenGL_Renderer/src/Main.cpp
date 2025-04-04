8097bc244abecdb963e659c9cf0a2171a5d3ce84
slaygony <rjs5785@hotmail.com>
2025-04-03 23:26:40 -0700

    got a basic setup for imgui and have a test window rendering

--- OpenGL_Renderer/src/Main.cpp
+++ OpenGL_Renderer/src/Main.cpp
@@ -1,5 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -16,9 +19,14 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+bool show_demo_window = true;
+bool show_another_window = false;
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 int main(void)
 {
 	GLFWwindow* window;
+	const char* glsl_version = "#version 300 es";
 
 	/* Initialize the library */
 	if (!glfwInit())
@@ -94,6 +102,15 @@ int main(void)
 
 	Renderer renderer;
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+	ImGui::StyleColorsDark();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
 	float r = 0.0f;
 	float increment = 0.5f;
 	/* Loop until the user closes the window */
@@ -101,6 +118,12 @@ int main(void)
 	{
 		/* Render here */
 		renderer.Clear();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		
 
 		shader.Bind();
 		shader.SetUniform4f("uColor", r, 0.3f, 0.8f, 1.0f);
@@ -115,12 +138,42 @@ int main(void)
 		r += increment;
 		std::cout << r << std::endl;
 
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui::EndFrame();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		/* Swap front and back buffers */
 		Call(glfwSwapBuffers(window));
 
 		/* Poll for and process events */
 		Call(glfwPollEvents());
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	//glfwDestroyWindow(window);
 
 	glfwTerminate();
 	return 0;
